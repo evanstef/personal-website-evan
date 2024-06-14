1de8142a84ffd8fe3cfd8b87a6e9e52899c396be
EvanSC <evangundala@gmail.com>
2024-06-14 16:32:34 +0700

    nambah dikit

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,24 @@ import Image from "next/image";
 import black from "../public/gambar.jpg";
 import MenuBooth from "./components/MenuBooth";
 import {motion} from "framer-motion";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const container = useRef(null)
+  
+  useGSAP(() => {
+    // animation image
+    const tl = gsap.timeline();
+    tl.from("#image", { y: -700, delay: 1, ease: "bounce", duration: 1 })
+      .to("#image", { duration: 1, y: 0, })
+      .to("#image", { duration: 0.7, rotateY: 360 })
+      .to("#image", { duration: 0.7, rotateX: 360 })
+    // animation end image
+  }, {scope: container});
+
+
 
   const text = "Front End Web Developer"
 
@@ -13,10 +29,10 @@ export default function Home() {
     <>
       <motion.div className="h-full -z-10" initial={{y: "-200vh"}}
       animate={{y:"0%"}} transition={{duration:0.7, delay : 0.2}}>
-      <div className="home-container h-full flex flex-col items-center mx-10 mt-10 md:flex-row md:gap-14 lg:mx-60">
-       <motion.span initial={{x:-200, opacity:0}} animate={{x:0, opacity:1}} transition={{delay:1}} className="md:w-1/2">
-          <Image className="w-80 h-80 rounded-2xl border-8 border-slate-400 shadow-2xl md:w-[440px] md:h-[440px] lg:w-full bg-cover bg-center" src={black} alt="" />
-       </motion.span>
+      <div ref={container} className="home-container h-full flex flex-col items-center mx-10 mt-10 md:flex-row md:gap-14 lg:mx-48">
+       <span className="md:w-1/2">
+          <Image id="image" className="w-80 h-80 rounded-full border-8 md:w-[450px] md:h-[450px] border-slate-400 shadow-2xl bg-cover bg-center" width={440} height={440} src={black} alt="" />
+       </span>
        <motion.div initial={{scale:0}} animate={{scale:1}} transition={{delay:1.2}} className=" mt-10 md:w-1/2">
           <h1 className="text-[45px] text-center font-bold lg:text-5xl">Crafting,Design,As a {text.split("").map((kata,i) => 
           <motion.span 
