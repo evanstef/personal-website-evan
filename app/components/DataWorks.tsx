89501effdd5f4e3232c624327fffe2a7eb59df95
EvanSC <evangundala@gmail.com>
2024-06-20 12:30:32 +0700

    nambah project sembilan

--- app/components/DataWorks.tsx
+++ app/components/DataWorks.tsx
@@ -8,6 +8,9 @@ import lima from '../../public/project-lima.png'
 import enam from '../../public/project-enam.png'
 import tujuh from '../../public/project-tujuh.png'
 import delapan from '../../public/delapan.png'
+import sembilan from '../../public/project-sembilan.png'
+import gsap from '../../public/download.jpeg'
+import three from '../../icon/three js.png'
 
 import Image from 'next/image'
 
@@ -28,6 +31,37 @@ import Image from 'next/image'
 
 
 export const data = [
+    {
+        id : 9, 
+        judul : "Animation 3D Model Web",
+        image : sembilan,
+        desc : "mengimplementasikan desain web 3D yang sudah saya pelajari hanya untuk sekedar latihan saja cara membuat web 3D jadi mungkin kedepannya saya akan membuat website yang ada unsur 3D saya menggunakkan webstie skectfab.com untuk mencari asset 3D model yang saya mau kendala saat membuat website ini adalah setup webgi yang agak rumit, namun saya ada kendala di bagian preview mode sehingga user bisa merotate 3d model tersebut pada saat di development masih berjalan lancar namun setelah naik ke production user tidak bisa merotate 3d model tersebut jadi, untuk sementara saya masih menonaktifkan fitur tersebut. dan mencoba debugging terlebih dahulu",
+        link : "https://vortex-gt-8.vercel.app/",
+        source : "https://github.com/evanstef/Animation-Web-3D",
+        tech : [
+            "NextJs",
+            "TailwindCSS",
+            "GSAP",
+            "Web GI(Three.js)",
+            "Typescript"
+         ],
+         icontech : [
+            // NextJS
+            <svg key={1} fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_950_641)"><path d="M11.2141 0.00645944C11.1625 0.0111515 10.9982 0.0275738 10.8504 0.039304C7.44164 0.346635 4.24868 2.18593 2.22639 5.01291C1.10029 6.58476 0.380059 8.36775 0.107918 10.2563C0.0117302 10.9156 0 11.1103 0 12.0041C0 12.898 0.0117302 13.0927 0.107918 13.7519C0.760117 18.2587 3.96716 22.0452 8.31672 23.4481C9.0956 23.6991 9.91672 23.8704 10.8504 23.9736C11.2141 24.0135 12.7859 24.0135 13.1496 23.9736C14.7613 23.7953 16.1267 23.3965 17.4733 22.7091C17.6798 22.6035 17.7196 22.5754 17.6915 22.5519C17.6727 22.5378 16.793 21.3578 15.7372 19.9314L13.8182 17.339L11.4135 13.7801C10.0903 11.8235 9.00176 10.2235 8.99238 10.2235C8.98299 10.2211 8.97361 11.8024 8.96891 13.7331C8.96188 17.1138 8.95953 17.2499 8.9173 17.3296C8.85631 17.4446 8.80938 17.4915 8.71085 17.5431C8.63578 17.5807 8.57009 17.5877 8.21584 17.5877H7.80997L7.70205 17.5197C7.63167 17.4751 7.58006 17.4164 7.54487 17.3484L7.4956 17.2428L7.50029 12.539L7.50733 7.83285L7.58006 7.74136C7.6176 7.69209 7.69736 7.62875 7.75367 7.59825C7.84985 7.55133 7.88739 7.54664 8.29325 7.54664C8.77185 7.54664 8.85161 7.5654 8.97595 7.70147C9.01114 7.73901 10.3132 9.7003 11.871 12.0628C13.4287 14.4252 15.5589 17.651 16.6053 19.2346L18.5056 22.1132L18.6018 22.0499C19.4534 21.4962 20.3543 20.7079 21.0674 19.8868C22.5853 18.1437 23.5636 16.0182 23.8921 13.7519C23.9883 13.0927 24 12.898 24 12.0041C24 11.1103 23.9883 10.9156 23.8921 10.2563C23.2399 5.74957 20.0328 1.96306 15.6833 0.560125C14.9161 0.311445 14.0997 0.140184 13.1848 0.036958C12.9595 0.0134976 11.4088 -0.0123089 11.2141 0.00645944ZM16.1267 7.26511C16.2393 7.32142 16.3308 7.42933 16.3636 7.54194C16.3824 7.60294 16.3871 8.90734 16.3824 11.8469L16.3754 16.0651L15.6317 14.9249L14.8856 13.7848V10.7185C14.8856 8.73608 14.895 7.62171 14.9091 7.56775C14.9466 7.43637 15.0287 7.33315 15.1413 7.27215C15.2375 7.22288 15.2727 7.21819 15.6411 7.21819C15.9883 7.21819 16.0493 7.22288 16.1267 7.26511Z" fill="black"/></g><defs><clipPath id="clip0_950_641"><rect fill="white" height="24" width="24"/></clipPath></defs></svg>,
+            
+            // TailwindCSS
+            <svg key={4} className='fill-sky-500' height="24" viewBox="0 0 24 24" width="24"  xmlns="http://www.w3.org/2000/svg"><path d="M18.5 9.51a4.22 4.22 0 0 1-1.91-1.34A5.77 5.77 0 0 0 12 6a4.72 4.72 0 0 0-5 4 3.23 3.23 0 0 1 3.5-1.49 4.32 4.32 0 0 1 1.91 1.35A5.77 5.77 0 0 0 17 12a4.72 4.72 0 0 0 5-4 3.2 3.2 0 0 1-3.5 1.51zm-13 4.98a4.22 4.22 0 0 1 1.91 1.34A5.77 5.77 0 0 0 12 18a4.72 4.72 0 0 0 5-4 3.23 3.23 0 0 1-3.5 1.49 4.32 4.32 0 0 1-1.91-1.35A5.8 5.8 0 0 0 7 12a4.72 4.72 0 0 0-5 4 3.2 3.2 0 0 1 3.5-1.51z"/></svg>,
+
+            // GSAP
+            <Image key={"asjndjanw"} src={gsap} alt="gsap"  width={24} height={24}/>,
+
+            // WebGI
+            <Image key={"aasdawnw"} src={three} alt="webgi"  width={24} height={24}/>,
+
+            // Typescript
+            <svg key={5} className='fill-sky-600' xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" viewBox="0 0 24 24"  xmlSpace="preserve" width="24" height="24"><g><path d="M21.656,0H2.344C1.05,0,0,1.05,0,2.344v19.312C0,22.95,1.05,24,2.344,24h19.312C22.95,24,24,22.95,24,21.656V2.344   C24,1.05,22.95,0,21.656,0z M13.922,13.316h-3.01v8.574H8.527v-8.574H5.531v-1.926h8.391V13.316z M21.698,20.37   c-0.467,0.835-1.325,1.305-2.238,1.5c-1.06,0.226-2.187,0.214-3.25,0.015c-0.521-0.098-0.972-0.244-1.353-0.44v-2.347   c0.426,0.357,0.888,0.625,1.387,0.803c0.499,0.179,1.003,0.268,1.511,0.268c0.45,0,0.951-0.056,1.339-0.304   c0.266-0.17,0.444-0.438,0.444-0.759c0-0.437-0.316-0.767-0.657-0.997c-0.499-0.336-1.072-0.555-1.625-0.785   c-0.822-0.342-1.434-0.76-1.838-1.254c-0.702-0.859-0.817-2.231-0.275-3.201c0.463-0.828,1.316-1.316,2.216-1.533   c0.981-0.236,2.046-0.236,3.041-0.081c0.423,0.066,0.813,0.167,1.17,0.304v2.193c-0.176-0.122-0.368-0.23-0.576-0.322   c-0.41-0.183-0.853-0.307-1.298-0.367c-0.445-0.06-0.914-0.07-1.353,0.033c-0.346,0.08-0.71,0.241-0.91,0.55   c-0.083,0.13-0.125,0.275-0.125,0.436c0,0.176,0.046,0.334,0.139,0.473c0.235,0.353,0.649,0.58,1.02,0.763   c0.652,0.32,1.336,0.56,1.962,0.931c0.608,0.359,1.148,0.859,1.412,1.525c0.127,0.32,0.191,0.693,0.191,1.118   C22.031,19.479,21.92,19.972,21.698,20.37z"/></g></svg>
+         ]
+    },
     {
         id : 8,
         judul : "Netflix Clone",
