07335ab08609ab00eede69d72f7fc5f49ec426ee
EvanSC <evangundala@gmail.com>
2024-06-26 23:55:13 +0700

    perbaikan sedikit

--- app/page.tsx
+++ app/page.tsx
@@ -28,11 +28,10 @@ export default function Home() {
     <>
       <motion.div className="h-full -z-10" initial={{y: "-200vh"}}
       animate={{y:"0%"}} transition={{duration:0.7, delay : 0.2}}>
-      <div ref={container} className="home-container h-full flex flex-col justify-center items-center mt-32 md:gap-14 lg:mx-44">
+      <div ref={container} className="home-container h-full mx-5 mt-40 md:gap-14 lg:mx-44">
        <motion.div initial={{scale:0}} animate={{scale:1}} transition={{delay:1.2}} className="mt-10 flex flex-col justify-center items-center">
-          <h1 className="text-[34px] font-bold lg:text-5xl mb-2 text-slate-500">Crafting, Design, As a</h1>
+          <h1 className="text-[30px] font-bold lg:text-5xl mb-2 text-slate-500">Crafting, Design, As a</h1>
           <div className="text-[37px] font-bold lg:text-5xl mb-4 text-slate-800 flex gap-2 md:gap-8">
-
             <div className="flex md:gap-3 text1">
               <p>F</p>
               <p>r</p>
@@ -61,7 +60,7 @@ export default function Home() {
             
           </div>
           
-          <h1 className="mx-5 hidden md:block md:mx-0 font-semibold lg:text-3xl"> 
+          <h1 className="mx-5 mb-1 hidden md:block md:mx-0 font-semibold lg:text-3xl"> 
             I do  
             <span className="inline-block ml-1">
               <Typewriter 
@@ -75,7 +74,7 @@ export default function Home() {
             /> 
             </span>
           </h1>
-          <p className="mx-5 my-3 hidden md:block md:mx-0 font-semibold lg:text-xl">Feel free to know me more by clicking the “about me” button below. Happy coding fellas!</p>
+          <p className="mx-5 mb-8 hidden md:block md:mx-0 font-semibold lg:text-xl">Feel free to know me more by clicking the “about me” button below. Happy coding fellas!</p>
           <div className="mx-5 md:mx-0 flex justify-center md:flex-none">
             <MenuBooth />
           </div>
